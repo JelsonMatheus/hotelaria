763b4169caf7e5b8d2212f72e2ef7777c639e5b0
ArielleSousa <ariellekellen7@gmail.com>
2022-01-27 23:37:53 -0300

    implementação da listagem de hoteis

--- app/controllers/admin/hotel_controller.rb
+++ app/controllers/admin/hotel_controller.rb
@@ -3,6 +3,8 @@ class Admin::HotelController < ApplicationController
     layout "admin"
     
     def index
+        @modulo = "Lista de Hotéis"
+        @hoteis = Hotel.all
     end
 
 end
--- app/helpers/admin/hotel_helper.rb
+++ app/helpers/admin/hotel_helper.rb
@@ -1,2 +1,6 @@
 module Admin::HotelHelper
+  def exibir_endereco(item)
+    "#{item.estado}/#{item.cidade}/#{item.rua}"
+  end
+
 end
